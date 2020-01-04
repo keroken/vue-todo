49e66f24f6c7a3626f7e645e98ff64ed392742cf
keroken <kenichi.shinagawa@gmail.com>
2020-01-04 22:38:49 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="input__container">
+        <h1>VueTodo</h1>
+        <input class="input__text" type="text" v-model="text" v-on:keyup.enter="createNewToDoItem" />
+        <button class="input__btn" @click="createNewToDoItem()"><i class="ion-ios-plus-outline"></i></button>
+    </div>
+    <ul class="list__container">
+        <TodoItem class="list__container" v-for="item in list" :item="item" @delete="deleteItem" @toggle="toggleItem" :key="item.id" />
+      </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoItem
+  },
+  data: () => {
+    return {
+      list: [],
+      text: '',
+    };
+  },
+  methods: {
+    createNewToDoItem() {
+      //validate todo
+      if (!this.text) {
+        alert("Please enter a todo!");
+        return;
+      }
+      this.list.push({ text: this.text, key: Date.now(), done: false });
+      todoStorage.save(this.list);
+      this.text = '';
+    },
+    deleteItem(key) {
+      this.list = this.list.filter(item => item.key !== key);
+      todoStorage.save(this.list);
+    },
+    toggleItem(key) {
+      const itemIndex = this.list.findIndex(item => { return item.key == key});
+      this.list[itemIndex].done = !this.list[itemIndex].done;
+      todoStorage.save(this.list);
+    },
+  },
+  created() {
+    this.list = todoStorage.fetch();
+  }
+}
+
+const STORAGE_KEY = 'todos-vuejs'
+const todoStorage = {
+  fetch: function() {
+    var list = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    return list
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
   }
 }
 </script>
@@ -23,6 +72,72 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 18px;
+  font-weight: 900;
+  color: #2c3e50;
+  height: 100vh;
+}
+
+.btn {
+  font-size: 18px;
+}
+
+.App {
+  text-align: center;
+}
+
+.app-title {
+  font-size: 28px;
+  color: deepskyblue;
+}
+
+.input__container {
+  height: 80px;
+  padding: 20px 10px;
+  background-color: greenyellow;
+}
+
+.input__text {
+  margin-top: 7px;
+  outline: none;
+  border: none;
+  border-radius: 3px;
+  padding: 5px 7px;
+  margin: 7px;
+}
+
+.input__btn {
+  font-size: 18px;
+  display: inline-block;
+  margin-left: 10px;
+  background-color: greenyellow;
+  border: none;
+  transition: all .2s;
+}
+
+.input__btn:hover {
+  transform: scale(1.2);
+}
+
+.input__btn:focus {
+  outline: none;
+}
+
+.list__container {
+  background-color: rgb(38, 228, 221);
+  list-style: none;
+}
+
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+  <li class="list__item">
+    <span class="list__item__span" v-bind:class="{ done: item.done }" @click="toggleItem(item.key)" >{{ item.text }}</span>
+    <button class="list__item--delete-btn">
+    <i class="btn ion-ios-close-outline" @click="deleteItem(item.key)" ></i></button>
+  </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['item'],
+  methods: {
+    deleteItem(key) {
+      this.$emit('delete', key);
+    },
+    toggleItem(key) {
+      this.$emit('toggle', key)
+    }
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.list__item {
+  padding: 10px;
+  border-top: 1px solid #eee;
+}
+
+.list__item--delete-btn {
+  margin-left: 10px;
+  background-color: rgb(38, 228, 221);
+  border: none;
+  transition: all .2s;
+}
+
+.list__item--delete-btn:hover {
+  transform: scale(1.2);
+}
+
+.list__item .done {
+  color: #888;
+  text-decoration: line-through;
+}
+
+</style>
